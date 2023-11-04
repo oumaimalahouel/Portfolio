9d736f4116ee1258eeb7c83aa86f9d76bc9c6054
Oumaima Lahouel <lahoueloumaima1@gmail.com>
2023-11-04 18:16:40 +0100

    Organisation des dossiers de projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import React , { useState } from 'react'
+
+import Hero from './components/2-hero/Hero'
+import Header from './components/1-header/Header'
+import Contact from './components/4-contact/Contact'
+import Footer from './components/5-footer/Footer'
+import Main from './components/3-main/main'
 
 
-import './App.css'
+
+
 
 function App() {
 
 
   return (
-    <>
-     
-      
-    </>
+    <div className='container'>
+     <Header />
+     <div className="divider"/>
+      <Hero />
+      <div className="divider"/>
+      <Main />
+      <div className="divider"/>
+      <Contact />
+      <div className="divider"/>
+      <Footer />
+    </div>
   )
 }
 
