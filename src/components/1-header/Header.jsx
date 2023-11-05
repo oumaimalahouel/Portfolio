5c4d6dc0acea3e61d36e5fbdf429a45ef6017bff
Oumaima Lahouel <lahoueloumaima1@gmail.com>
2023-11-05 05:37:42 +0100

    Responsive Header

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
-import './header.css';
+import { useState } from "react";
+import "./header.css";
 const Header = () => {
+  const [showModal, setshowModal] = useState(false);
   return (
-    <div>header</div>
-  )
-}
+    <header className="flex">
+      <button
+        onClick={() => setshowModal(true)}
+        className="menu icon-menu flex"
+      ></button>
+      <div />
+
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button className="mode flex">
+        <span className="icon-moon"></span>
+      </button>
+
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal">
+            <li>
+              <button
+                className="icon-close"
+                onClick={() => setshowModal(false)}
+              />
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Article</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Speaking</a>
+            </li>
+            <li>
+              <a href="">Uses</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
