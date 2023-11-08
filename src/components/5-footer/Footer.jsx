69f78731c015bbb71802692423d763fda5bcad5a
Oumaima Lahouel <lahoueloumaima1@gmail.com>
2023-11-08 15:26:59 +0100

    Responsive footer

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './footer.css';
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className='flex'>
+      <ul className='flex'>
+        <li><a href="">About</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Speaking</a></li>
+        <li><a href="">Uses</a></li>
+      </ul>
+    <p> © 2023 Spencer Sharp. All rights reserved.</p>
+
+    </footer>
   )
 }
 
