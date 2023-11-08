3e1815d3c386132a506f8e97949803ac1d86adb9
Oumaima Lahouel <lahoueloumaima1@gmail.com>
2023-11-08 14:46:28 +0100

    Responsive contact-us section

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -3,7 +3,33 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <div>contact</div>
+    <section className='contactUs'>
+      <h1 className='title'>
+        <span className='icon-envelope'></span>
+        Contact us
+      </h1>
+      <p className='subtitle'>Contact us for more information and Get notified when I publish somthing new.</p>
+   
+      <div className="flex">
+      <form>
+    <div className='flex'>
+    <label htmlFor="email">Email Adress : </label>
+    <input required type="email" id="email" />
+    </div>
+    <div className="flex" style={{marginTop:"24px"}}>
+    <label htmlFor="message">Your message:</label>
+    <textarea required name="" id="message" ></textarea>
+    </div>
+    <button className='submit'>Submit</button>
+      </form>
+      <div className="animation">animation</div>
+
+
+      </div>
+   
+   
+    </section>
+  
   )
 }
 
